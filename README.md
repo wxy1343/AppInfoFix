3427c3a3fafe8f9c026c7d26c03f03647c6ed612
wxy1343 <1343890272@qq.com>
2023-01-06 23:12:05 +0800

    first update

---
+++ README.md
@@ -0,0 +1,32 @@
+# Steam AppInfo 修复
+
+## AppInfo 是什么
+
+* 部分游戏`steam`账号没有游戏获取到的`AppInfo`不完整，导致`安装路径无效`和`应用配置无效`
+* `AppInfo`保存了`steam`游戏的基础配置，包括安装的目录，游戏进程名称，清单信息
+* `AppInfo`路径: `steam/appcache/appinfo.vdf`
+* 判断`AppInfo`是否完整: 打开[console](steam://open/console)输入`app_info_print {appid}`查找`depots`和`config`是否存在
+
+## 使用教程
+
+1. 确保已入库游戏，`depotcache`存在清单，可以使用[一键入库工具](https://github.com/wxy1343/ManifestAutoUpdate/releases/tag/storage)入库游戏
+2. 参数
+    * `-a, --app-id`: 游戏id
+    * `-i, --install-dir`: 安装目录
+    * `-l, --launch`: 启动进程
+    * `-d, --depot-id`: 仓库id
+    * `-m, --manifest-gid`: 清单id
+    * `-s, --size`: 仓库大小
+3. 使用示例
+    * 鬼谷八荒: `fix.exe -a 1468810 -i 鬼谷八荒 -l guigubahuang.exe -d 1468811 -m 6548132625584514585 -s 15503862689`
+    * 中国式家长: `fix.exe -a 736190 -i 中国式家长 -l game.exe`
+4. 注意事项
+    * `-d, -m, -s`参数可以按需指定，如果不存在会优先从本地清单获取`清单id`和`大小`信息，如果`-d`
+      参数不存在会从[清单仓库](https://github.com/wxy1343/ManifestAutoUpdate)获取信息
+    * 修复完成后需要重启`steam`，重启后迅速点击下载游戏，不然`steam`会更新`appinfo`信息，需要重新修复
+    * 下载完后如果无法运行报错`应用配置无效`还需要修复一遍
+
+## 参考项目
+
+* [leovp/steamfiles](https://github.com/leovp/steamfiles)
+* [tralph3/Steam-Metadata-Editor](https://github.com/tralph3/Steam-Metadata-Editor)
---
+++ fix.py
@@ -0,0 +1,108 @@
+import os
+import winreg
+import argparse
+import requests
+import traceback
+from VDF import VDF
+from pathlib import Path
+from steam.core.manifest import DepotManifest
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--app-id', type=int)
+parser.add_argument('-i', '--install-dir', type=str)
+parser.add_argument('-l', '--launch', type=str)
+parser.add_argument('-d', '--depot-id', type=str)
+parser.add_argument('-m', '--manifest-gid', type=str)
+parser.add_argument('-s', '--size', type=str)
+
+repo = 'wxy1343/ManifestAutoUpdate'
+
+
+def get_steam_path():
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Valve\Steam')
+    return Path(winreg.QueryValueEx(key, 'SteamPath')[0])
+
+
+def main(args=None):
+    args = parser.parse_args(args)
+    app_id = args.app_id or int(input('appid: '))
+    install_dir = args.install_dir or input('安装目录: ')
+    launch = args.launch or input('启动进程: ')
+    steam_path = get_steam_path()
+    vdf = VDF(steam_path=steam_path)
+    app_info = vdf.parsedAppInfo[app_id]
+    app_info['sections']['appinfo']['config'] = {'installdir': install_dir, 'launch': {'0': {'executable': launch}}}
+    depot_id_list = args.depot_id.split(',') if args.depot_id else []
+    manifest_gid_list = args.manifest_gid.split(',') if args.manifest_gid else len(depot_id_list) * [None]
+    size_list = args.size.split(',') if args.size else len(depot_id_list) * [None]
+    depot_cache = steam_path / 'depotcache'
+    manifest_info = dict()
+    for depot_id, manifest_gid, size in zip(depot_id_list, manifest_gid_list, size_list):
+        if not manifest_gid or not size:
+            if manifest_gid:
+                manifest_path = depot_cache / f'{depot_id}_{manifest_gid}.manifest'
+                if not manifest_path.exists():
+                    raise FileNotFoundError(manifest_path)
+                manifest_path = str(manifest_path.absolute())
+            else:
+                if not depot_cache.exists():
+                    raise FileNotFoundError('depotcache')
+                manifest_list = []
+                for i in depot_cache.iterdir():
+                    if i.suffix == '.manifest' and i.name.split('_')[0] == str(depot_id):
+                        manifest_list.append(i)
+                if not manifest_list:
+                    raise Exception(f'找不到清单: {depot_id}')
+                if len(manifest_list) > 1:
+                    manifest_list = sorted(manifest_list, key=lambda x: x.stat().st_mtime)
+                manifest_path = manifest_list[0]
+                manifest_path = str(manifest_path.absolute())
+            with open(manifest_path, 'rb') as f:
+                manifest = DepotManifest(f.read())
+            if not manifest_gid:
+                manifest_gid = str(manifest.gid)
+            size = str(manifest.metadata.cb_disk_original)
+        manifest_info[str(depot_id)] = (str(manifest_gid), str(size))
+    if not manifest_info:
+        manifest_list = []
+        url = f'https://api.github.com/repos/{repo}/branches/{app_id}'
+        r = requests.get(url)
+        if 'commit' in r.json():
+            sha = r.json()['commit']['sha']
+            url = r.json()['commit']['commit']['tree']['url']
+            r = requests.get(url)
+            if 'tree' in r.json():
+                for i in r.json()['tree']:
+                    if i['path'].endswith('.manifest'):
+                        manifest_list.append(i['path'])
+        if not depot_cache.exists():
+            raise FileNotFoundError('depotcache')
+        depot_cache_list = [i.name for i in depot_cache.iterdir()]
+        for manifest in manifest_list:
+            if manifest in depot_cache_list:
+                with (depot_cache / manifest).open('rb') as f:
+                    manifest = DepotManifest(f.read())
+                depot_id = str(manifest.depot_id)
+                manifest_gid = str(manifest.gid)
+                size = str(manifest.metadata.cb_disk_original)
+                manifest_info[depot_id] = (manifest_gid, size)
+    app_info['sections']['appinfo']['depots'] = {}
+    for depot_id, _ in manifest_info.items():
+        manifest_gid, size = _
+        print(f'depot: {depot_id}, manifests: {manifest_gid}, size: {size}')
+        app_info['sections']['appinfo']['depots'][str(depot_id)] = {'manifests': {'public': str(manifest_gid)},
+                                                                    'maxsize': str(size)}
+    vdf.update_app(app_info)
+    vdf.write_data()
+    print('appinfo.vdf修复成功')
+    print('重启steam生效')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
+    except:
+        traceback.print_exc()
+    os.system('pause')
